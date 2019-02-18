4d693d33891c6a2b4b05287d38586ec078891f41
Joohan Park <pajh0509@gmail.com>
2019-02-18 00:07:03 +0000

    joohan&jun integrated

---
+++ Assets/rogue.cs
@@ -0,0 +1,95 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class rogue : MonoBehaviour
+{
+
+    public float movePower = 1f;
+    public float jumpPower = 1f;
+
+    Rigidbody2D rigid;
+
+    Vector3 movement;
+    bool isJumping = false;
+
+    //---------------------------------------------------[Override Function]
+    //Initialization
+    void Start()
+    {
+        rigid = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    //Graphic & Input Updates	
+    void Update()
+    {
+        if (Input.GetButtonDown("Jump"))
+        {
+            isJumping = true;
+        }
+    }
+
+    //Physics engine Updates
+    void FixedUpdate()
+    {
+        Move();
+        Jump();
+    }
+
+    //---------------------------------------------------[Movement Function]
+
+    void Move()
+    {
+        Vector3 moveVelocity = Vector3.zero;
+
+        if (Input.GetAxisRaw("Horizontal") < 0)
+        {
+            moveVelocity = Vector3.left;
+        }
+
+        else if (Input.GetAxisRaw("Horizontal") > 0)
+        {
+            moveVelocity = Vector3.right;
+        }
+
+        transform.position += moveVelocity * movePower * Time.deltaTime;
+    }
+
+    void Jump()
+    {
+        if (!isJumping)
+            return;
+
+        //Prevent Velocity amplification.
+        rigid.velocity = Vector2.zero;
+
+        Vector2 jumpVelocity = new Vector2(0, jumpPower);
+        rigid.AddForce(jumpVelocity, ForceMode2D.Impulse);
+
+        isJumping = false;
+    }
+}
+
+
+
+
+
+
+//public class rogue : MonoBehaviour
+//{
+//    int jumpCount = 0;
+//    void Update()
+//    {
+//        if (Input.GetKeyDown(KeyCode.Space))
+//        {
+
+//        }
+//    }
+//    void OnCollisionEnter2D(Collision2D col)
+//    {
+//        jumpCount = 0;
+//    }
+
+//}
+
