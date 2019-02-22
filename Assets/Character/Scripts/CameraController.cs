eec5d1cc391a78878c1c67c0a8560cc17358b930
SeMin <ohsemin4@gmail.com>
2019-02-22 15:24:45 +0000

    merge_project

--- Assets/Character/Scripts/CameraController.cs
+++ Assets/Character/Scripts/CameraController.cs
@@ -1,57 +1,93 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
-public class CameraController : MonoBehaviour {
+public class CameraController : MonoBehaviour
+{
 
-    public Transform edge;
-    private CharacterBehavior player;
+
+    public static CameraController Instance;
 
     public GameObject Target;
-    public float PosY = 1;
-    private bool camera_move = true;
+    public int Smoothvalue = 10;
+    public float PosY = 0;
 
-    void Awake()
-    {
-        player = this.transform.root.Find("Player1").GetComponent<CharacterBehavior>();
-    }
+
+    // Use this for initialization
+    public Coroutine my_co;
+
     void Start()
     {
-        edge = this.transform.Find("LeftEdge");
+
     }
+
 
     void Update()
     {
-        float x_position = Target.transform.position.x;
-        float prev_x_poisition = edge.position.x;
+        Vector3 Targetpos = new Vector3(Target.transform.position.x, Target.transform.position.y + PosY, -100);
+        transform.position = Vector3.Lerp(transform.position, Targetpos, Time.deltaTime * Smoothvalue);
 
-        if(prev_x_poisition - x_position < -2.4)
-            camera_move = true;
-        else
-            camera_move = false;
 
-        // Debug.Log(prev_x_poisition - x_position);
-        
-        if(!camera_move){
-            if(player.is_ground){
-                Vector3 Targetpos = new Vector3(this.transform.position.x, Target.transform.position.y + PosY, -100);
-                transform.position = Vector3.Lerp(transform.position, Targetpos, Time.deltaTime * 4);
-             }else{
-                Vector3 Targetpos = new Vector3(this.transform.position.x, transform.position.y, -100);
-                transform.position = Vector3.Lerp(transform.position, Targetpos, Time.deltaTime * 1);
-             }
-        }else {
-            if(player.is_ground){
-                Vector3 Targetpos = new Vector3(Target.transform.position.x+4, Target.transform.position.y + PosY, -100);
-                transform.position = Vector3.Lerp(transform.position, Targetpos, Time.deltaTime * 4);
-            }else{
-                Vector3 Targetpos = new Vector3(this.transform.position.x+4, transform.position.y, -100);
-                transform.position = Vector3.Lerp(transform.position, Targetpos, Time.deltaTime * 1);
-             }
-        }
+
     }
 
 
 
 }
+//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEngine;
+//using System;
+
+//public class CameraController : MonoBehaviour {
+
+//    public Transform edge;
+//    private CharacterBehavior player;
+
+//    public GameObject Target;
+//    public float PosY = 1;
+//    private bool camera_move = true;
+
+//    void Awake()
+//    {
+//        player = this.transform.root.Find("Player1").GetComponent<CharacterBehavior>();
+//    }
+//    void Start()
+//    {
+//        edge = this.transform.Find("LeftEdge");
+//    }
+
+//    void Update()
+//    {
+//        float x_position = Target.transform.position.x;
+//        float prev_x_poisition = edge.position.x;
+
+//        if(prev_x_poisition - x_position < -2.4)
+//            camera_move = true;
+//        else
+//            camera_move = false;
+
+//        // Debug.Log(prev_x_poisition - x_position);
+
+//        if(!camera_move){
+//            if(player.is_ground){
+//                Vector3 Targetpos = new Vector3(this.transform.position.x, Target.transform.position.y + PosY, -100);
+//                transform.position = Vector3.Lerp(transform.position, Targetpos, Time.deltaTime * 4);
+//             }else{
+//                Vector3 Targetpos = new Vector3(this.transform.position.x, transform.position.y, -100);
+//                transform.position = Vector3.Lerp(transform.position, Targetpos, Time.deltaTime * 1);
+//             }
+//        }else {
+//            if(player.is_ground){
+//                Vector3 Targetpos = new Vector3(Target.transform.position.x+4, Target.transform.position.y + PosY, -100);
+//                transform.position = Vector3.Lerp(transform.position, Targetpos, Time.deltaTime * 4);
+//            }else{
+//                Vector3 Targetpos = new Vector3(this.transform.position.x+4, transform.position.y, -100);
+//                transform.position = Vector3.Lerp(transform.position, Targetpos, Time.deltaTime * 1);
+//             }
+//        }
+//    }
+
+
+
+//}
--- Assets/Character/Scripts/GroundSensor.cs
+++ Assets/Character/Scripts/GroundSensor.cs
@@ -7,7 +7,10 @@ public class GroundSensor : MonoBehaviour{
     private CharacterBehavior player;
     
     void Awake (){
-        player = this.transform.root.Find("Player1").GetComponent<CharacterBehavior>();
+        if(this.transform.root.name == "P1")
+            player = this.transform.root.Find("Player1").GetComponent<CharacterBehavior>();
+        else if(this.transform.root.name == "P2")
+            player = this.transform.root.Find("Player2").GetComponent<CharacterBehavior>();
     }
 
     void Start(){
@@ -25,6 +28,18 @@ public class GroundSensor : MonoBehaviour{
             // Debug.Log("ground");
         }
         if(other.tag == "Block")
+            player.jump_count = 0;
+    }
+    
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Ground")
+        {
+            player.is_ground = true;
+            player.jump_count = 0;
+            // Debug.Log("ground");
+        }
+        if (other.tag == "Block")
             player.jump_count = 0;
     }
 
