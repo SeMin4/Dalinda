78b9d4b24cf12b855929f48bc153b7529b6c1af1
SeMin <ohsemin4@gmail.com>
2019-03-04 09:56:16 +0000

    rebuild

---
+++ SourceCode/Assets/Character/Scripts/NorthAmerican.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+public class NorthAmerican : Skill
+{
+    private CharacterBehavior me;
+    private CharacterBehavior other_player;
+
+    public NorthAmerican(CharacterBehavior me, CharacterBehavior other)
+    {
+        skill_time = 10f;
+        this.me = me;
+        this.other_player = other;
+    }
+    // camera flip
+    public override void Skill1(bool t)
+    {
+        if(t)
+            other_player.camera_controller.camera_flip = true;
+        else
+            other_player.camera_controller.camera_flip = false;
+    }
+
+    // public void Skill2()
+    // {
+
+    // }
+
+
+}
---
+++ SourceCode/Assets/CharacterSelect/GameSetting.cs
@@ -0,0 +1,6 @@
+﻿public static class GameSetting
+{
+    public enum Characters {European, Korean, Egyptian, Cuban, Peruvian};
+    public static int selec_p1 = -1;
+    public static int selec_p2 = -1;
+}
